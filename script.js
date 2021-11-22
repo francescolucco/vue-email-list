c363a4052eafb8dda4b5ac506a95abfeee0edf8a
francescolucco <francescolucco@googlemail.com>
2021-11-22 16:57:44 +0100

    bonus

--- script.js
+++ script.js
@@ -4,22 +4,33 @@ const app = new Vue ({
 
    data: {
       mailList: [],
+      httpError: false,
+      isLoading: true
    },
 
    methods: {
       mailCreation(){
+         this.isLoading = true;
+         this.mailList = [];
+
          for(let i = 0; i < 10; i++){
+
             axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+
             .then((response)=>{
-               console.log(response);
                const mail = response.data.response;
                this.mailList.push(mail);
-               console.log(this.mailList);
+               if(this.mailList.length === 10){
+                  this.isLoading = false;
+               }
             })
+
             .catch((error)=>{
-               console.log(error);
+               console.log('KO', error);
+               this.httpError = true;
             })
          }
+         console.log(this.mailList);
       }
    },
 
@@ -28,72 +39,3 @@ const app = new Vue ({
    // },
 
 })
-
-
-
-
-
-
-
-
-
-
-
-// const app = new Vue ({
-
-//    el: '#app',
-
-//    data: {
-//       books: [],
-//       author: 'd/aquino',
-//    },
-
-//    methods: {
-//       getNumber(){
-//          axios.get('http://openlibrary.org/search.json?author='+this.author)
-//          .then((response)=>{
-//             console.log(response);
-//             this.books = response.data.docs;
-//             console.log(this.books);
-//          })
-//          .catch((error)=>{
-//             console.log(error);
-//          })
-//       }
-//    },
-
-//    mounted(){
-//       this.getNumber();
-//    }
-// })
-
-// NUMERO RANDOM DA 1 A 9
-// const app = new Vue ({
-
-//    el: '#app',
-
-//    data: {
-//       numero: 0,
-//       isError: false,
-//    },
-
-//    methods: {
-//       getNumber(){
-//          axios.get('https://flynn.boolean.careers/exercises/api/random/int')
-//          .then((response)=>{
-//             console.log(response);
-//             const data = response.data;
-//             console.log(data);
-//             this.numero = data.response;
-//          })
-//          .catch((error)=>{
-//             console.log(error);
-//             this.isError = true;
-//          })
-//       }
-//    },
-
-//    mounted(){
-//       this.getNumber();
-//    }
-// })
